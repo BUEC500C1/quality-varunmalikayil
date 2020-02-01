11fe99bcb4aa5d73c009e0e4515b69e7ebdd3d61
Varun Malikayil <var.malikayil@gmail.com>
2020-02-01 02:18:06 -0500

    adding fix

--- test_cases.py
+++ test_cases.py
@@ -2,13 +2,12 @@
 from RomanToArabic import RomanToArabic
 
 def test_answer():
-    # assert RomanToArabic.numberToRoman(1) == 'I'
-    # assert RomanToArabic.numberToRoman(40000) == 'Number too high, please enter a lower number'
-    # assert RomanToArabic.numberToRoman(-1) == 'Please enter a valid positive number'
-    # assert RomanToArabic.numberToRoman(3500) == 'MMMD'
-    # assert RomanToArabic.numberToRoman(453) == 'CDLIII'
-    # assert RomanToArabic.numberToRoman(239) == 'CCXXXIX'
-    assert 1 == 1
+    assert RomanToArabic.numberToRoman(1) == 'I'
+    assert RomanToArabic.numberToRoman(40000) == 'Number too high, please enter a lower number'
+    assert RomanToArabic.numberToRoman(-1) == 'Please enter a valid positive number'
+    assert RomanToArabic.numberToRoman(3500) == 'MMMD'
+    assert RomanToArabic.numberToRoman(453) == 'CDLIII'
+    assert RomanToArabic.numberToRoman(239) == 'CCXXXIX'
     
 
 
